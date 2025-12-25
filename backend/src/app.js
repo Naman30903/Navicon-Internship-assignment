0b674cc51d3e036db7d064c96a6b77b9bf98d718
Naman30903 <Namanjain30903@gmail.com>
2025-12-26 02:59:36 +0530

    feat: add Swagger documentation and UI integration for API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const tasksRoutes = require('./routes/tasks.routes');
 
 dotenv.config();
@@ -8,6 +11,15 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const swaggerPath = path.join(__dirname, '..', 'docs', 'swagger.yaml');
+const swaggerDocument = YAML.load(swaggerPath);
+
+app.get('/docs/swagger.yaml', (req, res) => {
+    return res.sendFile(swaggerPath);
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // basic health route
 app.get('/', (req, res) => res.json({ status: 'ok' }));
